d77a5aa8e9f5a807050b539a3df039176f34459f
Aishwarya Vishwanath <aishwaryavishwanath13@gmail.com>
2017-03-09 23:40:27 +0530

    Update checkNews.cs

--- MyBot/checkNews.cs
+++ MyBot/checkNews.cs
@@ -1,37 +1,126 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
-using System;
-using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MyBot
 {
-    [Serializable]
-    public class checkNews : IDialog<object>
+    public class CheckNews
     {
-        public async Task StartAsync(IDialogContext context)
+        newsObject newsObj = new newsObject();
+        Articles res = new Articles();
+        public string[] desc = new string[10];
+        public string[] title = new string[10];
+        public string[] url = new string[10];
+        public string[] urlToImage = new string[10];
+        public async Task getNews()
         {
-            context.Wait(MessageReceivedAsync);
-        }
-        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
-        {
-            var message = await argument;
-            PromptDialog.Confirm(context, askNews, "Are you sure?", "Didn't get that", promptStyle: PromptStyle.Auto);
-        }
-        private async Task askNews(IDialogContext context, IAwaitable<bool> result)
-        {
-            var confirm = await result;
-            if (confirm) // They said yes
+            using (var newsHttpClient = new HttpClient())
             {
-                string message = $"you can check the above links";
-                await context.PostAsync(message);
-                context.Wait(MessageReceivedAsync);
-            }
-            else // They said no
-            {
-                string msg = $"ok check them later!";
-                await context.PostAsync(msg);
-                context.Wait(MessageReceivedAsync);
+                string uri = $"https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey={ResourcesManager.newsAppID}";
+                HttpResponseMessage msg = await newsHttpClient.GetAsync(uri);
+                string sJson = string.Empty;
+                sJson = await msg.Content.ReadAsStringAsync();
+                newsObj = JsonConvert.DeserializeObject<newsObject>(sJson);
+                int i = 0;                
+                string[] store = new string[10];
+                if (null != newsObj)
+                {
+                    foreach (Articles r in newsObj.articles)
+                    {                                
+                        title[i] = r.title;
+                        desc[i] = r.description;
+                        url[i] = r.url;
+                        urlToImage[i] = r.urlToImage;
+                        i++;                                              
+                    }                    
+                }                
             }
         }
+    }  
+    public class newsObject
+    {
+        public string status { get; set; }
+        public string source { get; set; }
+        public string sortBy { get; set; }
+        public List<Articles> articles { get; set; }
     }
+    
+    public class Articles
+    {
+        public string author { get; set; }
+        public string description { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string urlToImage { get; set; }
+        public string publishedAt { get; set; }
+    }
+    
+}
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyBot
+{
+    public class CheckNews
+    {
+        newsObject newsObj = new newsObject();
+        Articles res = new Articles();
+        public string[] desc = new string[10];
+        public string[] title = new string[10];
+        public string[] url = new string[10];
+        public string[] urlToImage = new string[10];
+        public async Task getNews()
+        {
+            using (var newsHttpClient = new HttpClient())
+            {
+                string uri = $"https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey={ResourcesManager.newsAppID}";
+                HttpResponseMessage msg = await newsHttpClient.GetAsync(uri);
+                string sJson = string.Empty;
+                sJson = await msg.Content.ReadAsStringAsync();
+                newsObj = JsonConvert.DeserializeObject<newsObject>(sJson);
+                int i = 0;                
+                string[] store = new string[10];
+                if (null != newsObj)
+                {
+                    foreach (Articles r in newsObj.articles)
+                    {                                
+                        title[i] = r.title;
+                        desc[i] = r.description;
+                        url[i] = r.url;
+                        urlToImage[i] = r.urlToImage;
+                        i++;                                              
+                    }                    
+                }                
+            }
+        }
+    }  
+    public class newsObject
+    {
+        public string status { get; set; }
+        public string source { get; set; }
+        public string sortBy { get; set; }
+        public List<Articles> articles { get; set; }
+    }
+    
+    public class Articles
+    {
+        public string author { get; set; }
+        public string description { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string urlToImage { get; set; }
+        public string publishedAt { get; set; }
+    }
+    
 }
