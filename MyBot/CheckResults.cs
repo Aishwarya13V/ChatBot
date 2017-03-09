83b350250e7afa611eaee565976c7a8b8bfe0cf4
Aishwarya Vishwanath <aishwaryavishwanath13@gmail.com>
2017-03-09 23:43:14 +0530

    Add files via upload

---
+++ MyBot/CheckResults.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+using MyBot.FormFlow;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyBot
+{
+    public class CheckResults 
+    {
+        ConnectorClient connector = new ConnectorClient(new Uri("http://localhost:9000/api/messages"));
+        public async Task<string> getResult(string Qualification, string field, bool More, IDialogContext context)
+        {
+            string strRet = "";
+            string connectionString = "";
+            connectionString = "Data Source=VISHWANATH\\SQLSERVER2014;Initial Catalog=Bot_db;Integrated Security=true;MultipleActiveResultSets=true;User ID=Vishwanath; Password = ";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+
+            switch (Qualification)
+            {
+                case "Tenth":
+                    con.Open();
+                    cmd.Connection = con;
+                    cmd.CommandText = "select PreRequest from UG_PreReq where Field='" + field + "'";
+                    strRet += "  \nThese are the groups you can take in intermediate for " + field + " :  \n";
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        strRet += dr["PreRequest"] + " , ";
+                    }
+                    dr.Close();
+                    cmd.Dispose();
+                    con.Close();
+
+                    con.Open();
+                    cmd = new SqlCommand("select Exam_Name from Exams where Exam_ID IN (select Exam_ID from UG_Fields where Field = '" + field + "')", con);
+                    strRet += "  \nHere is the list of exams for graduation in " + field + " :  \n";
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        strRet += dr["Exam_Name"] + " , ";
+                    }
+                    dr.Close();
+                    cmd.Dispose();
+                    con.Close();
+
+                    //context.Wait(MessageReceivedAsync);
+                    //await StartAsync(context);
+                    if (More == true)
+                    {
+                        con.Open();
+                        cmd = new SqlCommand("select Exam_Name from Exams where Exam_ID IN (select Exam_ID from PG_Fields where Field = '" + field + "')", con);
+                        strRet += "  \nHere is the list of exams for post graduation in " + field + " :  \n";
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            strRet += dr["Exam_Name"] + " , ";
+                        }
+                        dr.Close();
+                        cmd.Dispose();
+                        con.Close();
+                    }
+                    //await context.PostAsync("Would you like to knoe more about these fields{||}");
+
+                    break;
+
+                case "Inter":
+                    con.Open();
+                    cmd = new SqlCommand("select * from UG_PreReq where Field='" + field + "'", con);
+                    strRet += "  \nThese are the prerequisites you need in intermediate for " + field + " :  \n";
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        strRet += dr["PreRequest"] + " , ";
+                    }
+
+                    dr.Close();
+                    cmd.Dispose();
+                    con.Close();
+                    if (field != null)
+                    {
+                        con.Open();
+                        cmd = new SqlCommand("select Exam_Name from Exams where Exam_ID IN (select Exam_ID from UG_Fields where Field = '" + field + "')", con);
+                        strRet += "  \nHere is the list of exams for graduation in " + field + " :  \n";
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            strRet += dr["Exam_Name"] + " , ";
+                        }
+                        dr.Close();
+                        cmd.Dispose();
+                        con.Close();
+                    }
+                    if (More == true)
+                    {
+                        con.Open();
+                        cmd = new SqlCommand("select Exam_Name from Exams where Exam_ID IN (select Exam_ID from PG_Fields where Field = '" + field + "')", con);
+                        strRet += "  \nHere is the list of exams for post graduation in " + field + " :  \n";
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            strRet += dr["Exam_Name"] + " , ";
+                        }
+                        dr.Close();
+                        cmd.Dispose();
+                        con.Close();
+                    }
+                    break;
+                case "Graduate":
+                    con.Open();
+                    cmd = new SqlCommand("select Specialization from  Specializations where Field = '" + field + "' ", con);
+                    strRet += "  \nSepcializations in " + field + " are :  \n";
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        strRet += dr["Specialization"] + " , ";
+                    }
+                    dr.Close();
+                    cmd.Dispose();
+                    con.Close();
+                    if (More == true)
+                    {
+                        con.Open();
+                        cmd = new SqlCommand("select Exam_Name from Exams where Exam_ID IN (select Exam_ID from PG_Fields where Field = '" + field + "')", con);
+                        strRet += "  \nHere is the list of exams for post graduation in " + field + " :  \n";
+                        dr = cmd.ExecuteReader();
+                        while (dr.Read())
+                        {
+                            strRet += dr["Exam_Name"] + " , ";
+                        }
+                        dr.Close();
+                        cmd.Dispose();
+                        con.Close();
+                    }
+                    break;
+            }
+            return strRet;
+
+        }
+        
+        /*public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+        }
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Would you like to know about the courses?");
+            PromptDialog.Confirm(context, Courses, sb.ToString(), "Didn't get that!");
+
+        }
+
+        private async Task Courses(IDialogContext context, IAwaitable<bool> result)
+        {
+            // Get the response from the user
+            var confirm = await result;
+            if (confirm) // They said yes
+            {
+                // Start a new Game
+                await context.PostAsync("Hi Welcome! - Guess a number between 1 and 10");
+                context.Wait(MessageReceivedAsync);
+            }
+            else // They said no
+            {
+                await context.PostAsync("Goodbye!");
+                context.Wait(MessageReceivedAsync);
+            }
+        }*/
+
+    }
+}
---
+++ MyBot/CheckWeather.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace MyBot
+{
+    public class CheckWeather
+    {
+        public async Task<string> getWeather(string query)
+        {
+            using (var newsHttpClient = new HttpClient())
+            {
+                string uri = $"https://api.apixu.com/v1/current.json?key={ResourcesManager.WMAppID}&q={query}";
+                HttpResponseMessage msg = await newsHttpClient.GetAsync(uri);
+
+                string sJson = string.Empty;
+                sJson = await msg.Content.ReadAsStringAsync();
+                weatherObject weatherObj = new weatherObject();
+                weatherObj = JsonConvert.DeserializeObject<weatherObject>(sJson);
+                string strRet = "";
+                var cur = weatherObj.current;
+                var con = weatherObj.current.condition;
+                if (null != cur)
+
+                {
+                    //strRet = "Temperature in celcius:" + cur.temp_c + "Condition:" + con.text + "Icon:" + con.icon +
+                             //"Humidity:" + cur.humidity + "Cloud:" + cur.cloud;
+                    string[] arr = { cur.temp_c.ToString(), con.text, con.icon, cur.humidity.ToString()};
+                    strRet = string.Join(",", arr);
+                }
+                return strRet;
+            }
+        }
+    }
+    public class weatherObject
+    {
+        public Location location { get; set; }
+        public Current current { get; set; }
+    }
+    public class Location
+    {
+        public string name { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public decimal lat { get; set; }
+        public decimal lon { get; set; }
+        public string tz_id { get; set; }
+        public int localtime_epoch { get; set; }
+        public string localtime { get; set; }
+    }
+    public class Current
+    {
+        public int last_updated_epoch { get; set; }
+        public string last_updated { get; set; }
+        public decimal temp_c { get; set; }
+        public decimal temp_f { get; set; }
+        public int isday { get; set; }
+        public Condition condition { get; set; }
+        public decimal wind_mph { get; set; }
+        public decimal wind_kph { get; set; }
+        public int wind_degree { get; set; }
+        public string wind_dir { get; set; }
+        public decimal pressure_mb { get; set; }
+        public decimal pressure_in { get; set; }
+        public decimal precip_mm { get; set; }
+        public decimal precip_in { get; set; }
+        public int humidity { get; set; }
+        public int cloud { get; set; }
+        public decimal feelslike_c { get; set; }
+        public decimal feelslike_f { get; set; }
+    }
+    public class Condition
+    {
+        public string text { get; set; }
+        public string icon { get; set; }
+        public int code { get; set; }
+    }
+}
