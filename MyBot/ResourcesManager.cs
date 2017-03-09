43c3fabf1791944d397a13aef018b5736efc7992
Aishwarya Vishwanath <aishwaryavishwanath13@gmail.com>
2017-03-09 23:41:32 +0530

    Update ResourcesManager.cs

--- MyBot/ResourcesManager.cs
+++ MyBot/ResourcesManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +8,11 @@ namespace MyBot
     public class ResourcesManager
     {
         public static readonly string LUISAppID = "23ec4ca7-e8b6-4e08-a40f-d4f3919b264a";
-        public static readonly string LUISSubscriptionKey = "YOUR_LUIS_SUB_KEY";
+        //public static readonly string LUISSubscriptionKey = "YOUR_LUIS_SUB_KEY";
 
         //public static readonly string MSTranslatorBasicAuth = "MICROSOFT_BING_TRANSLATOR_BASIC_AUTH_KEY";
-        public static readonly string OWMAppID = "OPENWEATHERMAP_APP_ID";
+        public static readonly string WMAppID = "f7c290d97b4c4cc6887100418170802";
+        public static readonly string newsAppID = "03bafdb5720641c4bf3836c8017a1d1b";
 
     }
 }
