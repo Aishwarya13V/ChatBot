83580e8db53ec8fd9cf7791a253f40cf31f3a4fd
Aishwarya Vishwanath <aishwaryavishwanath13@gmail.com>
2017-03-09 23:46:11 +0530

    Add files via upload

---
+++ MyBot/Enquiry.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using System;
+
+namespace MyBot.FormFlow
+{
+    [Serializable]
+    public class Enquiry
+    {
+        [Prompt("What is your current qualification?{||}")]
+        public Qualification qualification { get; set; }
+        [Prompt("ok.That's good..!! Which of the following field you are interested in/or currently pursuing?{||}")]
+        public Field field { get; set; }
+        [Prompt("Would you like to know about post graduation details in this field?")]
+        public bool More { get; set; }
+
+        public enum Qualification
+        {
+            other, Tenth, Inter, Graduate
+        }
+        public enum Field
+        {
+            Other, Engineering, Medicine, Law
+        }
+        public static IForm<Enquiry> BuildEnquiryForm()
+        {
+            CheckResults Result = new CheckResults();
+            Prompting prompt = new Prompting();
+            return new FormBuilder<Enquiry>()
+                .Message("If you have difficulty in answering please enter 'help'")
+                /*.Field(new FieldReflector<Enquiry>(nameof(Enquiry.field))
+                    .SetPrompt(new PromptAttribute("What type of engines does the ship have? {||}")))*/
+                .OnCompletion(async (context, Enquiry) =>
+                {
+                    // Set BotUserData
+                    context.PrivateConversationData.SetValue<bool>(
+                        "ProfileComplete", true);
+                    context.PrivateConversationData.SetValue(
+                        "Qualification", Enquiry.qualification.ToString());
+                    context.PrivateConversationData.SetValue(
+                        "field", Enquiry.field.ToString());
+                    context.PrivateConversationData.SetValue<bool>(
+                       "More Information", Enquiry.More);
+                    // Tell the user that the form is complete
+                    /*FormButton fb = new FormButton();
+                    fb.Message = "Would you like to know more about these fields";
+                    fb.Title = "More";*/
+                    string result =  await Result.getResult(Enquiry.qualification.ToString(), Enquiry.field.ToString(), Enquiry.More, context);
+                    await context.PostAsync("I guess this is the suitable path according to your interest" + result);
+                    //await context.PostAsync("Would you like to know more about these fields");
+                })
+                .Build();
+        }
+    }
+}
---
+++ MyBot/Prompting.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyBot.FormFlow
+{
+    [Serializable]
+    public class Prompting
+    {
+        //[Prompt("would you like to know more about this field?{||}")]
+        public bool More { get; set; }
+        public static IForm<Prompting> BuildPromptingForm()
+        {
+            Prompting prompt = new Prompting();
+            return new FormBuilder<Prompting>()
+                .Field(new FieldReflector<Prompting>(nameof(Prompting.More))
+                    .SetPrompt(new PromptAttribute("would you like to know more about this field?{||}")))
+                .OnCompletion(async (context, Prompting) =>
+                {
+                    context.PrivateConversationData.SetValue<bool>(
+                       "More Information", Prompting.More);
+                    string result = await prompt.field_Desc(Prompting.More);
+                    await context.PostAsync(result);
+
+                })
+                
+                .Build();
+        }
+        public async Task<string> field_Desc(bool More)
+        {
+            string strRet = " ";
+            string connectionString = "";
+            connectionString = "Data Source=VISHWANATH\\SQLSERVER2014;Initial Catalog=Bot_db;Integrated Security=true;MultipleActiveResultSets=true;User ID=Vishwanath; Password = ";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+            if (More == true)
+            {
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "select PreRequest from UG_PreReq";
+                strRet += "  \nThese are the groups you can take in intermediate for  :  \n";
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    strRet += dr["PreRequest"] + " , ";
+                }
+                dr.Close();
+                cmd.Dispose();
+                con.Close();
+            }
+            return strRet;
+        }
+
+    }
+}
