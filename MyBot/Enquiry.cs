0e308fae9ece2f68b3cdac93212c0a44b91d8247
Aishwarya Vishwanath <aishwaryavishwanath13@gmail.com>
2017-03-14 09:49:50 +0530

    Update Enquiry.cs

--- MyBot/Enquiry.cs
+++ MyBot/Enquiry.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.FormFlow;
 using Microsoft.Bot.Builder.FormFlow.Advanced;
+using Microsoft.Bot.Connector;
 using System;
 
 namespace MyBot.FormFlow
@@ -8,25 +9,29 @@ namespace MyBot.FormFlow
     [Serializable]
     public class Enquiry
     {
+        internal int count = 0;
+        internal string save;
         [Prompt("What is your current qualification?{||}")]
         public Qualification qualification { get; set; }
         [Prompt("ok.That's good..!! Which of the following field you are interested in/or currently pursuing?{||}")]
         public Field field { get; set; }
         [Prompt("Would you like to know about post graduation details in this field?")]
         public bool More { get; set; }
-
         public enum Qualification
         {
-            other, Tenth, Inter, Graduate
+            Other,Tenth, Inter, Graduate
         }
         public enum Field
         {
             Other, Engineering, Medicine, Law
         }
+
         public static IForm<Enquiry> BuildEnquiryForm()
         {
             CheckResults Result = new CheckResults();
+            Enquiry enquiry = new Enquiry();
             Prompting prompt = new Prompting();
+
             return new FormBuilder<Enquiry>()
                 .Message("If you have difficulty in answering please enter 'help'")
                 /*.Field(new FieldReflector<Enquiry>(nameof(Enquiry.field))
@@ -42,15 +47,15 @@ namespace MyBot.FormFlow
                         "field", Enquiry.field.ToString());
                     context.PrivateConversationData.SetValue<bool>(
                        "More Information", Enquiry.More);
-                    // Tell the user that the form is complete
-                    /*FormButton fb = new FormButton();
-                    fb.Message = "Would you like to know more about these fields";
-                    fb.Title = "More";*/
-                    string result =  await Result.getResult(Enquiry.qualification.ToString(), Enquiry.field.ToString(), Enquiry.More, context);
-                    await context.PostAsync("I guess this is the suitable path according to your interest" + result);
-                    //await context.PostAsync("Would you like to know more about these fields");
+                    // Tell the user that the form is complete                    
+                    string result = await Result.getResult(Enquiry.qualification.ToString(), Enquiry.field.ToString(), Enquiry.More, context);
+                    Enquiry.save = Enquiry.field.ToString();                    
+                    await context.PostAsync("I guess this is the suitable path according to your interest" + result, "");
+                    
+                    //await Conversation.SendAsync(activity, () => { return Chain.From(() => FormDialog.FromForm(Prompting.BuildPromptingForm)); });
+                    //await Conversation.SendAsync(Enquiry.activity, () => new NumberGuesserDialog());
                 })
                 .Build();
         }
-    }
+    }   
 }
